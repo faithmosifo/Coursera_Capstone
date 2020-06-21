7adf17d97d2b294a954d5ed44546b5813463b262
faithmosifo <60079774+faithmosifo@users.noreply.github.com>
2020-06-21 16:39:16 -0500

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This will mainly be used for my capstone project.
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print("Hello Capstone project course!")
+
+
+# In[ ]:
+
+
+
+
